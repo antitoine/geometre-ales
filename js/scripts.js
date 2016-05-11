f752b08445466c2fb23a7b3590db9b3b7a521e04
Antoine Chabert <chabert.antoine@gmail.com>
2016-05-11 21:41:46 +0200

    Add header

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+  /***************** Header BG Scroll ******************/
+
+  $(function() {
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 200) {
+        $('header .navbar')
+          .addClass('navbar-fixed-top')
+          .addClass('navbar-light')
+          .addClass('bg-faded')
+          .removeClass('navbar-dark')
+          .removeClass('bg-inverse');
+        $('section.content').css({
+          "padding-top": "60px"
+        });
+      } else {
+        $('header .navbar')
+          .removeClass('navbar-fixed-top')
+          .addClass('navbar-dark')
+          .addClass('bg-inverse')
+          .removeClass('navbar-light')
+          .removeClass('bg-faded');
+        $('section.content').css({
+          "padding-top": "0"
+        });
+      }
+    });
+  });
+  /***************** Smooth Scrolling ******************/
+
+  $(function() {
+
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+
+  });
+
+});
