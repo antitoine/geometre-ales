f9114144af4520a93a7d22781bc3b9e5472ddb70
Antoine Chabert <chabert.antoine@gmail.com>
2016-05-12 10:25:14 +0200

    Use Waypoint to fixed the main menu and add effect on it

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,8 @@ $(document).ready(function() {
   /***************** Header BG Scroll ******************/
 
   $(function() {
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-
-      if (scroll >= 460) {
-        $('header .navbar')
-          .addClass('navbar-fixed-top')
-          .addClass('navbar-light')
-          .addClass('bg-faded')
-          .removeClass('navbar-dark')
-          .removeClass('bg-inverse');
-        $('header .heading').css({
-          "padding-top": "60px"
-        });
-      } else {
+    $('body > .content').waypoint(function(direction) {
+      if (direction === 'up') {
         $('header .navbar')
           .removeClass('navbar-fixed-top')
           .addClass('navbar-dark')
@@ -26,9 +14,22 @@ $(document).ready(function() {
         $('header .heading').css({
           "padding-top": "0"
         });
+      } else if (direction === 'down') {
+        $('header .navbar')
+          .fadeOut(0)
+          .addClass('navbar-fixed-top')
+          .addClass('navbar-light')
+          .addClass('bg-faded')
+          .removeClass('navbar-dark')
+          .removeClass('bg-inverse')
+          .fadeIn(500);
+        $('header .heading').css({
+          "padding-top": "60px"
+        });
       }
     });
   });
+  
   /***************** Smooth Scrolling ******************/
 
   $(function() {
