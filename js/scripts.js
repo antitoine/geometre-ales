c91014e8cd897ddf1b865f5093c59b0c0c1da59d
Antoine Chabert <chabert.antoine@gmail.com>
2016-05-12 11:01:07 +0200

    Remove retina.js and add images corresponding to the screen size with media queries in CSS

--- js/scripts.js
+++ js/scripts.js
@@ -2,52 +2,68 @@ $(document).ready(function() {
 
   /***************** Header BG Scroll ******************/
 
-  $(function() {
-    $('body > .content').waypoint(function(direction) {
-      if (direction === 'up') {
-        $('header .navbar')
-          .removeClass('navbar-fixed-top')
-          .addClass('navbar-dark')
-          .addClass('bg-inverse')
-          .removeClass('navbar-light')
-          .removeClass('bg-faded');
-        $('header .heading').css({
-          "padding-top": "0"
-        });
-      } else if (direction === 'down') {
-        $('header .navbar')
-          .fadeOut(0)
-          .addClass('navbar-fixed-top')
-          .addClass('navbar-light')
-          .addClass('bg-faded')
-          .removeClass('navbar-dark')
-          .removeClass('bg-inverse')
-          .fadeIn(500);
-        $('header .heading').css({
-          "padding-top": "60px"
-        });
-      }
-    });
+  $('body > .content').waypoint(function(direction) {
+    if (direction === 'up') {
+      $('header .navbar')
+        .removeClass('navbar-fixed-top')
+        .addClass('navbar-dark')
+        .addClass('bg-inverse')
+        .removeClass('navbar-light')
+        .removeClass('bg-faded');
+      $('header .heading').css({
+        "padding-top": "0"
+      });
+    } else if (direction === 'down') {
+      $('header .navbar')
+        .fadeOut(0)
+        .addClass('navbar-fixed-top')
+        .addClass('navbar-light')
+        .addClass('bg-faded')
+        .removeClass('navbar-dark')
+        .removeClass('bg-inverse')
+        .fadeIn(500);
+      $('header .heading').css({
+        "padding-top": "60px"
+      });
+    }
   });
-  
+
   /***************** Smooth Scrolling ******************/
 
-  $(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
 
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html,body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
       }
-    });
-
+    }
   });
 
+  /********** Dynamic elements apparitions *************/
+
+  $('.dynamic-presentation').waypoint(function() {
+    $('.dynamic-presentation').addClass('animated fadeInUp');
+  }, {
+    offset: '75%'
+  });
+  $('.dynamic-activities').waypoint(function() {
+    $('.dynamic-activities').addClass('animated fadeInRight');
+  }, {
+    offset: '75%'
+  });
+  $('.dynamic-references').waypoint(function() {
+    $('.dynamic-references').addClass('animated fadeInLeft');
+  }, {
+    offset: '75%'
+  });
+  $('.dynamic-contact').waypoint(function() {
+    $('.dynamic-contact').addClass('animated fadeInDown');
+  }, {
+    offset: '75%'
+  });
 });
